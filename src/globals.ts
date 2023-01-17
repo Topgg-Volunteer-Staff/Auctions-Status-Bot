4b080841302f9ab161fc13b363f92b9aba591b48
pixxies <hello@pixxiesdesign.com>
2023-01-17 17:50:54 +0100

    Update role and channel ID

--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,5 @@
-const announcementChannelId = '1062691931337011280'
+const announcementChannelId = '1064940583266820106'
 
-const pingRoleId = '1008414779787989005'
+const pingRoleId = '1064947337207746640'
 
 export { announcementChannelId, pingRoleId }
