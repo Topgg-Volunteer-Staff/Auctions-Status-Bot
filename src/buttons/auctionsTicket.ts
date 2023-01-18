23959159afdf0a4d79fae4004a236f081ecf12fe
pixxies <hello@pixxiesdesign.com>
2023-01-18 17:05:43 +0100

    Add ticketing system

---
+++ src/buttons/auctionsTicket.ts
@@ -0,0 +1,89 @@
+import {
+  ButtonInteraction,
+  ChannelType,
+  Client,
+  EmbedBuilder,
+  TextChannel,
+} from 'discord.js'
+import { channelIds, roleIds } from '../globals'
+import { emoji } from '../utils/emojis'
+import { errorEmbed, successEmbed } from '../utils/embeds'
+
+export const button = {
+  name: 'auctionsTicket',
+}
+
+export const execute = async (
+  _client: Client,
+  interaction: ButtonInteraction
+) => {
+  if (!interaction.inCachedGuild()) return
+
+  const auctionsTickets = interaction.client.channels.cache.get(
+    channelIds.auctionsTickets
+  ) as TextChannel
+  const openTicket =
+    (await auctionsTickets.threads.fetchActive()).threads.filter(
+      (t) =>
+        t.name ===
+        `${interaction.user.username}-${interaction.user.discriminator}`
+    ).size >= 1
+      ? true
+      : false
+
+  if (openTicket) {
+    const yourTicket = (await auctionsTickets.threads.fetchActive()).threads
+      .filter(
+        (t) =>
+          t.name ===
+          `${interaction.user.username}-${interaction.user.discriminator}`
+      )
+      .first()
+    return interaction.reply({
+      embeds: [
+        errorEmbed(
+          `Can't open a new ticket!`,
+          `You already have an open Auctions support ticket. Please go to <#${yourTicket?.id}> for support.`
+        ),
+      ],
+      ephemeral: true,
+    })
+    return
+  }
+  const embed = new EmbedBuilder()
+    .setTitle(
+      `This is your Private Top.gg Auctions Support Thread, ${interaction.user.username}!`
+    )
+    .setDescription(
+      `Please state your question or what you are having an issue with in this thread.\n\n${emoji.dotred} If your issue is related to payments you have made, please make sure to include your FastSpring order ID starting with \`DBOTSBV••••\`. You can find the invoice ID in the payment confirmation email you received from FastSpring.\n\nA Support Team member will be with you as soon as possible!`
+    )
+    .setColor('#ff3366')
+
+  const channel = interaction.client.channels.cache.get(
+    channelIds.auctionsTickets
+  ) as TextChannel
+
+  channel.threads
+    .create({
+      name: `${interaction.user.username}-${interaction.user.discriminator}`,
+      autoArchiveDuration: 1440,
+      type: ChannelType.PrivateThread,
+    })
+    .then((thread) => {
+      thread.send({
+        content: `<@&${roleIds.supportTeam}>, <@${interaction.user.id}> has created an Auctions ticket.`,
+        embeds: [embed],
+      })
+
+      interaction.reply({
+        embeds: [
+          successEmbed(
+            `Ticket opened!`,
+            `Your ticket has been created at <#${thread.id}>, please head there for assistance!`
+          ),
+        ],
+        ephemeral: true,
+      })
+    })
+  return
+}
---
+++ src/commands/resolve.ts
@@ -0,0 +1,54 @@
+import {
+  ChannelType,
+  Client,
+  CommandInteraction,
+  SlashCommandBuilder,
+} from 'discord.js'
+import { channelIds, resolvedFlag } from '../globals'
+import { errorEmbed, successEmbed } from '../utils/embeds'
+import { emoji } from '../utils/emojis'
+
+export const command = new SlashCommandBuilder()
+  .setName('resolve')
+  .setDescription('Mark this auctions ticket as resolved')
+  .setDMPermission(false)
+  .setDefaultMemberPermissions('0')
+
+export const execute = async (
+  _client: Client,
+  interaction: CommandInteraction
+) => {
+  if (interaction.channel?.type !== ChannelType.PrivateThread)
+    return interaction.reply({
+      embeds: [errorEmbed('This is not a thread!')],
+      ephemeral: true,
+    })
+  if (interaction.channel.parent?.id !== channelIds.auctionsTickets)
+    return interaction.reply({
+      embeds: [errorEmbed(`This thread is not under #auctions-tickets!`)],
+      ephemeral: true,
+    })
+
+  if (interaction.channel.name.startsWith(resolvedFlag))
+    return interaction.reply({
+      embeds: [errorEmbed(`This ticket is already resolved!`)],
+      ephemeral: true,
+    })
+
+  // We can't rename threads due to rate limits
+  //   interaction.channel
+  //     .setName(`${resolvedFlag} ${interaction.channel.name})
+  //     .catch(console.error)
+
+  interaction.channel.setAutoArchiveDuration(4320, 'Ticket resolved!')
+
+  interaction.reply({
+    embeds: [
+      successEmbed(
+        `Ticket Resolved!`,
+        `If your issue persists or if you need help with a separate issue, please open a new ticket in <#${channelIds.auctionsTickets}>!\n\nThank you for using Top.gg Auctions! ${emoji.topggthumbsup}`
+      ),
+    ],
+  })
+  return
+}
