f6b26663e5d6771fb87c1c6b5bfdb56ffeeba697
Spark2120 <97990619+Spark2120@users.noreply.github.com>
2025-06-08 13:42:33 -0400

    time push

--- src/utils/embeds/auctionsMessages/adsNowLive.ts
+++ src/utils/embeds/auctionsMessages/adsNowLive.ts
@@ -7,25 +7,24 @@ import {
 } from 'discord.js'
 import { emoji } from '../../emojis'
 
-const nextMonday = new Date()
-// checks if today isnt monday
-if (nextMonday.getDay() !== 1) {
-  nextMonday.setDate(
-    nextMonday.getDate() + ((8 - nextMonday.getDay()) % 7 || 7)
-  )
-}
-nextMonday.setHours(19)
-nextMonday.setMinutes(0)
+    const now = new Date();
 
-// checks if today isnt tuesday
-const nextTuesday = new Date()
-if (nextTuesday.getDay() !== 2) {
-  nextTuesday.setDate(
-    nextTuesday.getDate() + ((9 - nextTuesday.getDay()) % 7 || 7)
-  )
-}
-nextTuesday.setHours(19)
-nextTuesday.setMinutes(0)
+    const getNextUtcDate = (targetDay: number, hourUTC: number) => {
+        const result = new Date(now);
+        const currentDay = now.getUTCDay();
+
+        let daysUntilTarget = targetDay - currentDay;
+        if (daysUntilTarget < 0 || (daysUntilTarget === 0 && now.getUTCHours() >= hourUTC)) {
+            daysUntilTarget += 7;
+        }
+
+        result.setUTCDate(now.getUTCDate() + daysUntilTarget);
+        result.setUTCHours(hourUTC, 0, 0, 0);
+        return result;
+    };
+    
+    const tuesday19UTC = getNextUtcDate(2, 19); // Tuesday @ 19:00 UTC
+    const tuesday20UTC = getNextUtcDate(2, 20); // Tuesday @ 20:00 UTC
 
 export const adsNowLive: BaseMessageOptions = {
   embeds: [
@@ -34,11 +33,11 @@ export const adsNowLive: BaseMessageOptions = {
       .setColor('#ff3366')
       .setDescription(
         `This week's winning auctions bids are starting to go live and will run until <t:${Math.floor(
-          nextTuesday.getTime() / 1000
-        )}:f> (<t:${Math.floor(nextTuesday.getTime() / 1000)}:R>)
+          tuesday20UTC.getTime() / 1000
+        )}:f> (<t:${Math.floor(tuesday20UTC.getTime() / 1000)}:R>)
         \n\n[Bidding is now open](https://auctions.top.gg) for next week's auctions and will end on<t:${Math.floor(
-          nextMonday.getTime() / 1000
-        )}:f> (<t:${Math.floor(nextMonday.getTime() / 1000)}:R>)!
+          tuesday19UTC.getTime() / 1000
+        )}:f> (<t:${Math.floor(tuesday19UTC.getTime() / 1000)}:R>)!
         \n\nThanks for using Top.gg Auctions! ${emoji.topggthumbsup}`
       ),
   ],
