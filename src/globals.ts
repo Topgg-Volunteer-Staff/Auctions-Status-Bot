897ddcb73a312914d1fc4062014b3b7132cd1fef
Spark2120 <97990619+Spark2120@users.noreply.github.com>
2025-08-01 19:05:04 -0400

    modals

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,7 @@
 const channelIds = {
-  auctionsTickets: '1012032743250595921',
+  auctionsTickets: '1369280368238395394',
   auctionsStatus: '1064940583266820106',
-  modTickets: '1285771377160491049',
+  modTickets: '1369280368238395394',
 }
 
 const roleIds = {
