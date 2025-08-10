eca1722cf4289a648682b65f72ae21c98014728c
Spark2120 <97990619+Spark2120@users.noreply.github.com>
2025-08-10 08:35:40 -0400

    prettier

--- src/modals/modModal.ts
+++ src/modals/modModal.ts
@@ -194,11 +194,11 @@ export const execute = async (
     messageContent = '[No details provided]'
   }
 
-const sentMessage = await webhook.send({
-  content: messageContent,
-  threadId: thread.id,
-});
-await sentMessage.pin();
+  const sentMessage = await webhook.send({
+    content: messageContent,
+    threadId: thread.id,
+  })
+  await sentMessage.pin()
   await webhook.delete()
 
   await interaction.editReply({
