bb1ad7d79e9b256f6923d2b98f7978cdab6ed20f
Spark2120 <97990619+Spark2120@users.noreply.github.com>
2025-08-11 20:25:38 -0400

    added ping

--- src/buttons/closeModTicket.ts
+++ src/buttons/closeModTicket.ts
@@ -42,7 +42,7 @@ export const execute = async (
   try {
     await interaction.reply({
       content:
-        'This ticket has been locked and archived. Still need help? Create another ticket in <#1285771377160491049>',
+        `This ticket has been locked and archived by <@!${interaction.user.id}>. Still need help? Create another ticket in <#1285771377160491049>`,
       ephemeral: false,
     })
 
