0d829f16397e37001d14f5e81e6df1e960556bf9
Spark2120 <97990619+Spark2120@users.noreply.github.com>
2025-08-12 23:19:12 -0400

    test

--- src/buttons/other.ts
+++ src/buttons/other.ts
@@ -23,7 +23,7 @@ export const execute = async (
 
   const reasonInput = new TextInputBuilder()
     .setCustomId('modReason')
-    .setLabel('What 𝖽o 𝗒ou 𝗇eed 𝗁elp 𝗐ith?')
+    .setLabel('What 𝖽o 𝗒ou 𝗇eed 𝗁elp 𝗐ith sir?')
     .setStyle(TextInputStyle.Paragraph)
     .setRequired(true)
     .setMaxLength(1000)
