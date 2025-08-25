d7d1d61febcae8e3e0d2f36a06462566dc16e061
IXZ7799 <ixzwolf7799@gmail.com>
2025-08-25 13:11:25 +0100

    fix

--- src/menus/info_bot_review.ts
+++ src/menus/info_bot_review.ts
@@ -10,8 +10,6 @@ export const execute = async (
 ) => {
   if (!interaction.inCachedGuild()) return
 
-  await interaction.update({})
-
   const embed = new EmbedBuilder()
     .setColor('#E91E63')
     .setTitle('When will my bot be reviewed?')
@@ -19,5 +17,5 @@ export const execute = async (
       "**Our current average review time is 1-2 weeks.**\n\nSome bots may take longer to review than others depending on their features. Because of this, we can't guarantee your bot will be reviewed as quickly as someone else's, and there's no exact timeframe for approval. There's also no way to check your position in the queue — but remember, you're not first or last!\n\nYou're free to edit your bot's page anytime, both before and after review. This won't affect your place in the queue.\n\nYou can read more about our review process here: [How the Reviewing Process Works.](https://support.top.gg/support/solutions/articles/73000502501-how-the-bot-reviewal-process-works)\n\n<:topgg_ico_bulb:1026877525261033563> In the meantime, please make sure your bot follows all of our [Bot Guidelines](https://support.top.gg/support/solutions/articles/73000502502-bot-guidelines) for a quick and smooth approval!"
     )
 
-  await interaction.followUp({ embeds: [embed], ephemeral: true })
+  await interaction.reply({ embeds: [embed], ephemeral: true })
 }
--- src/menus/info_server_review.ts
+++ src/menus/info_server_review.ts
@@ -10,8 +10,6 @@ export const execute = async (
 ) => {
   if (!interaction.inCachedGuild()) return
 
-  await interaction.update({})
-
   const embed = new EmbedBuilder()
     .setColor('#E91E63')
     .setTitle('When will my server be reviewed?')
@@ -19,5 +17,5 @@ export const execute = async (
       '**There is no set review time for servers.**\n\nIf your server does not get approved within a few minutes after submitting, it means it failed our automoderator checks.\n\nPlease make sure your server follows all of our [Server Guidelines](https://support.top.gg/support/solutions/articles/73000502503-server-guidelines) for a quick and smooth approval!\n\nNote: you must delete and re-add your server to get it reviewed again if it fails our initial checks.'
     )
 
-  await interaction.followUp({ embeds: [embed], ephemeral: true })
+  await interaction.reply({ embeds: [embed], ephemeral: true })
 }
