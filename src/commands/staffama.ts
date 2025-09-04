02a399078aba39053f7be98c3fdb4627b548b4b8
William <97990619+sudowilliam@users.noreply.github.com>
2025-09-04 12:20:05 -0400

    Empheral Changes (#42)
    * commit
    
    * changes for now
    
    * changes for now
    
    * push
    
    * removed webhook for disputes, not needed
    
    * test
    
    * test again
    
    * many changes for the better xoxo
    
    * added message link
    
    * message link
    
    * keeps fetching until it finds it
    
    * final changes
    
    * prettier
    
    * fixed compiling issue
    
    * fix some texts to match staff
    
    * enhanced reviewer logic + pretty fix
    
    * reduce chars
    
    * pretty fix
    
    * allow mentions
    
    * Update disputeDecline.ts
    
    * remove emoji and add dispute to thread name
    
    * update create mod ticket text
    
    * added reviewer role id
    
    * changes + fixes
    
    * fixes
    
    * text changes
    
    * ping fix
    
    * Update src/modals/disputeDecline.ts
    
    Co-authored-by: Dylan Spaenij <92450933+spaenij@users.noreply.github.com>
    
    * Update src/modals/disputeDecline.ts
    
    * prettier
    
    * button colours set to grey
    
    * report btns on top row
    
    * menus
    
    * reset menu test
    
    * revert change
    
    * reset option added
    
    * Update reset.ts
    
    * Update createModTicket.ts
    
    * Update reset.ts
    
    * Update reset.ts
    
    * capitalise staff names
    
    * testing fonts
    
    * fix texts
    
    * format fix
    
    * Update dispute_decline.ts
    
    * ama
    
    * fixed ownership transfer ticket wording
    
    * prettier
    
    * test new dispute
    
    * testing reset
    
    * Update createModTicket.ts
    
    * info embed for bot/serv review
    
    * change file names + add project status info
    
    file names cuz it was driving me crazy wdym reportbot_bot pls 😭
    
    * update name
    
    * Update createModTicket.ts
    
    * last one i promise
    
    * new contact user
    
    * Update contactUserModal.ts
    
    * Update contactUserModal.ts
    
    * fix
    
    * fix
    
    * Update dispute_decline.ts
    
    * Update contactUserModal.ts
    
    * Update contactUserModal.ts
    
    * turn contactuser into command instead
    
    * format
    
    * Update createModTicket.ts
    
    * Update createAuctionsTicket.ts
    
    * Update createAuctionsTicket.ts
    
    * Revert "Update createAuctionsTicket.ts"
    
    This reverts commit 201ebdb899543787a173c2cc3767ba803e4785c9.
    
    * Revert "Update createAuctionsTicket.ts"
    
    This reverts commit 1120b53bb9d50a0c054d3fd0dfbc84596342359f.
    
    * fixed registration issue
    
    * auction time pushes
    
    * ephermal update
    
    * fixed cron
    
    * prettier
    
    * fixed compile errors i hope
    
    * this should fix it up
    
    * removed accidental period that was placed
    
    * changes
    
    * Update info_projectstatus.ts
    
    * format
    
    * guild registration
    
    * prettier
    
    * work please
    
    * prettier
    
    * fix
    
    * removed empheral from close button
    
    * ama changes + prettier
    
    * ama
    
    * dispute decline fix
    
    * update text
    
    * Update staffama.ts
    
    added link to event
    
    ---------
    
    Co-authored-by: Spark2120 <97990619+Spark2120@users.noreply.github.com>
    Co-authored-by: IXZ7799 <ixzwolf7799@gmail.com>
    Co-authored-by: Dylan Spaenij <92450933+spaenij@users.noreply.github.com>

--- src/commands/staffama.ts
+++ src/commands/staffama.ts
@@ -10,6 +10,7 @@ import {
   MessageFlags,
   SlashCommandBuilder,
 } from 'discord.js'
+import { emoji } from '../utils/emojis'
 
 export const command = new SlashCommandBuilder()
   .setName('staff-ama')
@@ -28,10 +29,13 @@ export const execute = async (
   if (!channel || !(channel instanceof TextChannel)) return
 
   const panelEmbed = new EmbedBuilder()
-    .setTitle('📢 Staff Ask Me Anything Event')
+    .setTitle(`<:topgg_ico_microphone:1026877531296649256> Top.gg Public AMA`)
     .setDescription(
-      'Click the button below to submit a question for the AMA!\n\n' +
-        'Please only ask questions about Top.gg, its features, or things you would like to see on the site.'
+      `Ask your question(s) for our upcoming **Public AMA**, hosted <t:1757163600:F> in **[Stage](https://discord.com/channels/264445053596991498/829020599200383016)**!\n\n` +
+        `Ever wanted to know what we have on our roadmap, how Top.gg is doing, or have personal questions for our Team? Now is your chance to ask them! ${emoji.pog}\n\n` +
+        `Submit your questions by <t:1757163600:F>. Please note that we can't answer all questions!\n\n` +
+        `> Set your reminder for our AMA here: https://discord.gg/dbl?event=1411780560442884337\n\n` +
+        `**We look forward to seeing you and answering your question(s)!** ${emoji.onion}`
     )
     .setColor('#ff3366')
 
