0d495abdae739d43ce81a8b07d43b6661b4df57f
IXZ7799 <ixzwolf7799@gmail.com>
2025-10-25 14:07:03 +0100

    test

--- src/modals/modModal.ts
+++ src/modals/modModal.ts
@@ -176,7 +176,6 @@ export const execute = async (
       const categoryLabels: Record<string, string> = {
         account: 'Account Issue',
         ban_appeal: 'Ban Appeal',
-        bug: 'Bug Report',
         project_listing: 'Project Listing Issue',
         unable_to_vote: 'Unable to Vote Issue',
         other: 'Other Issue',
