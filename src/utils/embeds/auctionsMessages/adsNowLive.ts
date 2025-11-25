df08f6e0f23090545e3f181f0181b17b616927d6
Luke Stoodley <luke@ichigo.uk>
2025-11-25 20:37:56 +0000

    chore: prettier

--- src/utils/embeds/auctionsMessages/adsNowLive.ts
+++ src/utils/embeds/auctionsMessages/adsNowLive.ts
@@ -1,12 +1,11 @@
-
 import {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
   EmbedBuilder,
   BaseMessageOptions,
-} from 'discord.js';
-import { emoji } from '../../emojis';
+} from 'discord.js'
+import { emoji } from '../../emojis'
 
 /**
  * TODO: rewrite this to make it simpler:
@@ -16,29 +15,31 @@ import { emoji } from '../../emojis';
  *  - BIDDING ENDS: 7pm UTC
  *  - NEW BIDDING STARTS: 8pm UTC
  *  - PAYMENT WINDOW ENDS: 7pm UTC
- *  - 
+ *  -
  *  - BID REMOVAL ENDS: 6:50pm UTC
  */
 
 // Main builder
 export const adsNowLive = async (): Promise<BaseMessageOptions> => {
   // Use UTC for all event times
-  const now = new Date();
-  const year = now.getUTCFullYear();
-  const month = now.getUTCMonth();
-  const day = now.getUTCDate();
+  const now = new Date()
+  const year = now.getUTCFullYear()
+  const month = now.getUTCMonth()
+  const day = now.getUTCDate()
 
   // Helper to get unix timestamp for UTC date
   function getUnixUTC(daysFromNow: number, hour: number, minute = 0) {
-    const date = new Date(Date.UTC(year, month, day + daysFromNow, hour, minute, 0));
-    return Math.floor(date.getTime() / 1000);
+    const date = new Date(
+      Date.UTC(year, month, day + daysFromNow, hour, minute, 0)
+    )
+    return Math.floor(date.getTime() / 1000)
   }
 
   // Events
   // Ads run until next Tuesday: 7 days from now, 20:00 UTC
-  const adsEndUnix = getUnixUTC(7, 19, 0);
+  const adsEndUnix = getUnixUTC(7, 19, 0)
   // Bidding ends next Monday: 6 days from now, 19:00 UTC
-  const biddingEndUnix = getUnixUTC(6, 19, 0);
+  const biddingEndUnix = getUnixUTC(6, 19, 0)
 
   return {
     embeds: [
@@ -63,5 +64,5 @@ export const adsNowLive = async (): Promise<BaseMessageOptions> => {
           .setEmoji('1062691484471660625')
       ),
     ],
-  };
-};
+  }
+}
