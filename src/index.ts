2b806e57c3a5abe2be4d8a000b0875d030bbd6b1
Spark2120 <97990619+Spark2120@users.noreply.github.com>
2026-02-02 20:57:48 -0500

    fixed mod channel ID

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import { channelIds } from './globals'
 import { threadAlerts } from './commands/alert'
 import { updateThreadActivity } from './utils/tickets/trackActivity'
 
-const FOUR_IMAGE_LOG_CHANNEL_ID = '1409605702628016261'
+const FOUR_IMAGE_LOG_CHANNEL_ID = '396848636081733632'
 const fourImageFlagCounts = new Map<string, number>()
 
 function isImageAttachment(attachment: {
